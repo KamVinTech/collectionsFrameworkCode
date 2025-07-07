b090626e950ad799cc7b5b723b0fa034d8942e18
Sachin <comppride@gmail.com>
2025-07-07 19:26:10 +0530

    Made Changes to List and Set packages - Second commit.

--- DataStructures/collection/List/ArraylistBasic.java
+++ DataStructures/collection/List/ArraylistBasic.java
@@ -1,5 +1,84 @@
 package DataStructures.collection.List;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class ArraylistBasic {
+
+    public static final String result_File="arrayList-output-txt";
+    private static final boolean ALSO_ECHO_ON_CONSOLE=true;
+
+    public static void arrayListOperations(){
+        List<String> aList= new ArrayList<>();
+        Scanner sc= new Scanner(System.in);
+       
+        try(
+            PrintWriter out= new PrintWriter(new FileWriter(result_File,true))
+            ){
+                java.util.function.Consumer<String> log =s ->{
+                    out.println(s);
+                    if(ALSO_ECHO_ON_CONSOLE)
+                    {
+                        System.out.println(s);
+                    }
+                };
+                log.accept("====Starting ArrayList Operations====");
+
+    
+       
+        aList.add("Computer Science");
+        aList.add("Electrical");
+        aList.add("Electronics");
+        aList.add("Mechanical");
+        aList.add("Civil");
+        aList.add("Aeronautics");
+        log.accept("Initial List: " + aList);
+
+        aList.add(1,"Robotics");
+        log.accept("After Inserting 'Robotics' at index 1 :" + aList);
+
+        aList.remove("Electrical");
+        log.accept("After removing 'electrical' : " +aList);
+
+        aList.set(2,"Chemical");
+        log.accept("After updating index 2 to 'Chemical' : " + aList);
+
+        log.accept("Searching for an Element: ");
+        String Search= sc.nextLine();
+        if(aList.contains(Search)){
+            log.accept(Search + " is present at Index " + aList.indexOf(Search));
+        }
+        else{
+            log.accept(Search + "is not in the list.");
+        }
+
+        log.accept("Iterating using the for-each loop: ");
+        for(String engg:aList){
+            log.accept(engg);
+        }
+
+        Collections.sort(aList);
+        log.accept("Sorted List : " + aList);
+        
+        log.accept("====End of Operations====\n");
+
+    } catch (IOException e) {
+        System.err.println("File Writing Error: "+ e.getMessage());
+    }finally{
+        sc.close();
+    }
+        
+    }
+    public static void main(String[] args) {
+        arrayListOperations();
+
+
+    }
+
 
 }
---
+++ DataStructures/collection/Set/HashsetBasic.java
@@ -0,0 +1,80 @@
+package DataStructures.collection.Set;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class HashsetBasic {
+
+    private static final String set_result_file="set-output.txt";
+    private static final boolean ALSO_ECHO_ON_CONSOLE=true;
+
+    public static void performSetOperations(){
+
+        Set<String> set= new HashSet<>();
+        Scanner sc= new Scanner(System.in);
+
+        try(
+            PrintWriter out= new PrintWriter(new FileWriter(set_result_file,true))
+            ){
+                java.util.function.Consumer<String> log =s -> {
+                    out.println(s);
+                    if (ALSO_ECHO_ON_CONSOLE)
+                    {
+                        System.out.println(s);
+                    } 
+                };
+                    log.accept("====Set (HashSet) Operations====");
+                     
+                    set.add("Dog");
+                    set.add("Cat");
+                    set.add("Parrot");
+                    set.add("cock");
+                    set.add("cat");
+                    log.accept("Initial Elements of Set : " + set);
+
+                    log.accept("Enter an animal name to remove from the set :");
+                    String toRemove= sc.nextLine();
+                    if(set.remove(toRemove)){
+                        log.accept(toRemove + "was removed from the set.");
+                    }else{
+                        log.accept(toRemove + "was not found in the set");
+                    }
+
+                    log.accept("Current Set : " + set);
+
+                    log.accept("Enter an animal name to check whether it exists or not :");
+                    String search =  sc.nextLine();
+                    if(set.contains(search)){
+                        log.accept(search + "Animal exist in the set.");
+                    }else{
+                        log.accept(search + "Animal doesn't exist in the set.");
+                    }                    
+
+                    log.accept("Iterating through set: ");
+                    for(String animal:set){
+                        System.out.println(animal);
+                    }
+
+                    List<String> sortedList= new ArrayList<>(set);
+                    Collections.sort(sortedList);
+                    log.accept("Sorted Set (Via List) :" + sortedList);
+                    log.accept("====End of Operations====");
+
+                } catch (IOException e) {
+                    System.err.println("Error writing to file: " +e.getMessage());
+                }
+            }
+
+    public static void main(String[] args) {
+        performSetOperations();
+    }
+
+
+}
